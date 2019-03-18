cd9f88f80b6e586ef23316dc3b04c9003167bfda
Pavlov18 <topor001@mail.ru>
2019-03-18 14:54:45 +0400

    Финиш скриптов (кроме карты)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 			var buttonwrite = document.querySelector(".button-write-me");
 			var popupform = document.querySelector(".modal-message");
 			var closeWrite = popupform.querySelector(".modal-write-close");
@@ -10,7 +11,6 @@
 			var fullname = popupform.querySelector("[name=fullname]");
 			var email = popupform.querySelector("[name=email]");
 			var message = popupform.querySelector("[name=message]")
-			var i;
 			var isStorageSupport = true;
 			var storageName = "";
 			var storageEmail = "";
@@ -37,7 +37,6 @@
 				} else {
 					fullname.focus();
 				}
-
 			});
 
 			closeWrite.addEventListener("click", function (evt) {
@@ -46,7 +45,7 @@
 				popupform.classList.remove("modal-error");
 			});
 
-			for (i = 0; i < buttonsBuy.length; i++) {
+			for (var i = 0; i < buttonsBuy.length; i++) {
 				var buttonBuy = buttonsBuy[i];
 				buttonBuy.addEventListener('click', function (evt) {
 					evt.preventDefault();
@@ -54,12 +53,11 @@
 				});
 			}
 
-			for (i = 0; i < closeAddCart.length; i++) {
+			for (var i = 0; i < closeAddCart.length; i++) {
 				var buttonClose = closeAddCart[i];
 				buttonClose.addEventListener("click", function (evt) {
 					evt.preventDefault();
 					popupcart.classList.remove("modal-show");
-
 				});
 			}
 
@@ -77,19 +75,16 @@
 					}
 				}
 			});
-
-
+			
 			window.addEventListener("keydown", function (evt) {
 				if (evt.keyCode === 27) {
 					evt.preventDefault();
-					if (popupform.classList.contains("modal-show")) {
-						popup.classList.remove("modal-show");
-						popup.classList.remove("modal-error");
+					var modShow = document.querySelectorAll(".modal-show");
+					for (var i = 0; i < modShow.length; i++) {
+						modShow[0].classList.remove("modal-show");
 					}
 				}
 			});
-
-
 
 			var slider = document.querySelector(".slider");
 			var slides = slider.querySelectorAll(".slide");
@@ -97,12 +92,12 @@
 			var btnBack = slider.querySelector(".slider-button-back");
 			var btnNext = slider.querySelector(".slider-button-next");
 			var sliderContent = slider.querySelector(".slider-content");
+			var firstSlide = sliderContent.firstElementChild;
+			var finalSlide = sliderContent.lastElementChild;
 
 			btnBack.addEventListener("click", function (evt) {
 				var currentSlide = sliderContent.querySelector(".current");
 				var prevSlide = currentSlide.previousElementSibling;
-				var firstSlide = sliderContent.firstElementChild;
-				var finalSlide = sliderContent.lastElementChild;
 				currentSlide.classList.remove("current");
 				currentSlide.classList.add("visually-hidden");
 				if (currentSlide != firstSlide) {
@@ -116,12 +111,8 @@
 			});
 
 			btnNext.addEventListener("click", function (evt) {
-
 				var currentSlide = sliderContent.querySelector(".current");
 				var nextSlide = currentSlide.nextElementSibling;
-				var firstSlide = sliderContent.firstElementChild;
-				var finalSlide = sliderContent.lastElementChild;
-
 				currentSlide.classList.remove("current");
 				currentSlide.classList.add("visually-hidden");
 				if (currentSlide != finalSlide) {
@@ -134,65 +125,46 @@
 				}
 			});
 
-
-
-
 			btnsNetxPrev = slider.querySelector(".buttons-next-prev").querySelectorAll("button");
-			for (i = 0; i < btnsNetxPrev.length; i++) {
+			for (var i = 0; i < btnsNetxPrev.length; i++) {
 				btnsNetxPrev[i].addEventListener("click", function (evt) {
-				evt.preventDefault();
+					evt.preventDefault();
+					for (var i=0; i!= slides.length; ++i) {
 
-				for (var i=0; i!= slides.length; ++i) {
-					if (slides[i].classList.contains("current")) {
-						var nNum = i+1;
-						console.log(nNum + " - Определили номер сайда")
-						for (var i = 0; i < slideRadio.length; i++) {       
-							if (slideRadio[i].checked) {
-								slideRadio[i].checked = false;
+						if (slides[i].classList.contains("current")) {
+							var nNum = i+1;
+							for (var i = 0; i < slideRadio.length; i++) {       
+								if (slideRadio[i].checked) {
+									slideRadio[i].checked = false;
+								}
 							}
-						}
-						var parent = slider.querySelector(".slider-indicators");
-						var elems = parent.querySelectorAll("li");
-						for (var i = 0; i < elems.length; i++) {
-							var NumLiButton = i+1;
-							if (NumLiButton === nNum) {
+							var parent = slider.querySelector(".slider-indicators");
+							var elems = parent.querySelectorAll("li");
+							for (var i = 0; i < elems.length; i++) {
+								var NumLiButton = i+1;
+								if (NumLiButton === nNum) {
 									elems[NumLiButton - 1].querySelector("input").checked = true;
+								}
 							}
-						}
-						return;		
-					};
-				}
+							return;		
+						};
+					}
 				});
 			}
 
 			for (var i=0 ; i<slideRadio.length; i++) {
 				slideRadio[i].addEventListener("click", function(){
-
 					for (var i = 0; i < slideRadio.length; i++) {       
 						if (slideRadio[i].checked) {
 							numRadio = slideRadio[i].value;
-
-		for (var i = 0; i < slides.length; i++) {           // visually-hidden всех слайдов и удаление .current
-			slides[i].classList.add("visually-hidden");  
-			slides[i].classList.remove("current");
-		}
-
-		var numSlide = slider.querySelector(".slide:nth-child(" + numRadio + ")");
-		numSlide.classList.remove("visually-hidden");
-		numSlide.classList.add("current");
-	}
-}
-
-});
+							for (var i = 0; i < slides.length; i++) {
+								slides[i].classList.add("visually-hidden");  
+								slides[i].classList.remove("current");
+							}
+							var numSlide = slider.querySelector(".slide:nth-child(" + numRadio + ")");
+							numSlide.classList.remove("visually-hidden");
+							numSlide.classList.add("current");
+						}
+					}
+				});
 			}
-
-
-
-
-
-
-
-
-
-
-
